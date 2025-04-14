c10fe13124e337debc582064b4753ad0a343f0d7
dev <dev@devui-MacBookPro.local>
2025-04-14 16:06:29 +0900

    #21/Test: 환경변수 이름 변경

--- Web/app/manage/post/page.js
+++ Web/app/manage/post/page.js
@@ -864,7 +864,7 @@ const StepNavigation = ({ currentStep, totalSteps, onNext, onPrev, onComplete, i
         {currentStep === totalSteps && (
             <button onClick={async () => {
                 try {
-                    const response = await fetch(`${process.env.GCP_API_URL}/generate?name=진스키친&address=춘천&time=12시&number=010-1111-2222&description=춘천+양식+맛집`);
+                    const response = await fetch(`${process.env.NEXT_PUBLIC_GCP_API_URL}/generate?name=진스키친&address=춘천&time=12시&number=010-1111-2222&description=춘천+양식+맛집`);
                     const result = await response.json();
                     setGeneratedData(result);
                     console.log(result);
