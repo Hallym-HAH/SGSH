f17ea20d53259c9d57911519bda240c85dbd74e9
dev <dev@devui-MacBookPro.local>
2025-05-10 20:34:22 +0900

    #43/feat: 대시보드 추가

--- Web/app/manage/page.js
+++ Web/app/manage/page.js
@@ -1,16 +1,17 @@
 'use client'
 
-import ManageInfo from "@/components/feature/manage_info";
+import ManageDashboard from "@/components/feature/manage_dashboard";
 import { usePathname } from "next/navigation";
+
 
 export default function Manage({ children }) {
     const pathname = usePathname();
     return (
         <>
-            <div className="flex flex-row min-h-screen bg-[#f9fafb]">
+            <div className="flex flex-row min-h-screen bg-[#f5f6ff]">
                 <div className="w-full">
                     {pathname == ("/manage") ?
-                        <ManageInfo />
+                        <ManageDashboard />
                         : null}
                     {children}
                 </div>
