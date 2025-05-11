71289c85b90f3b343f01bd744ad4f4f103905328
dev <dev@devui-MacBookPro.local>
2025-05-12 08:56:27 +0900

    #46/feat: 오류 수정

--- Web/app/manage/page.js
+++ Web/app/manage/page.js
@@ -3,19 +3,34 @@
 import ManageDashboard from "@/components/feature/manage_dashboard";
 import { usePathname } from "next/navigation";
 
-
 export default function Manage({ children }) {
     const pathname = usePathname();
+
+    // 조건 배열
+    const managePaths = [
+        "/manage/info",
+        "/manage/menu",
+        "/manage/order",
+        "/manage/reserve",
+        "/manage/sales",
+        "/manage/post"
+    ];
+
+    // 해당 문자열로 시작하면 true
+    const isManagePath = managePaths.some(path => pathname.startsWith(path));
+
+    // /manage 또는 /manage/ 인지 체크
+    const isManageRoot = pathname === "/manage" || pathname === "/manage/";
+
     return (
-        <>
-            <div className="flex flex-row min-h-screen bg-[#f5f6ff]">
-                <div className="w-full">
-                    {pathname == ("/manage") ?
-                        <ManageDashboard />
+        <div className="flex flex-row min-h-screen bg-[#f5f6ff]">
+            <div className="w-full">
+                {isManageRoot
+                    ? <ManageDashboard />
+                    : isManagePath
+                        ? children
                         : null}
-                    {children}
-                </div>
             </div>
-        </>
+        </div>
     );
 }
